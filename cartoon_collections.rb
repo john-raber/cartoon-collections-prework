5203981b2894ad84881ea834ecbe0ad4a0a4515a
John Raber <jrab224@gmail.com>
2018-06-03 17:33:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,11 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
+  array.map 
   
+  array.collect do |call|
+    call << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
